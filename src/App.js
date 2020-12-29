34d4c066946e8424647e65ab889137feeb764d3e
Rachid Rezzik <rachid.rezzik@hotmail.com>
2020-12-29 11:56:51 -0600

    finished desktop, still needs responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
+import Currency from './components/Currency';
 
 function App() {
+  const [currency, setCurrency] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+      axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then((res) => {
+        setCurrency(res.data)
+        console.log(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+    window.scrollTo({
+      top: 0
+    })
+  }
+
+  const filteredCurrency = currency.filter(currency => 
+    currency.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search_div">
+        <h1>Cryptocurrencies</h1>
+        <form>
+          <input type="text" placeholder="Search Crypto" onChange={handleChange} />
+        </form>
+        <div className="currency_row" style={{border: "none", maxWidth: "950px"}}>
+                <div className="currency">
+                    <p style={{width:"25px"}}></p>
+                    <p style={{marginRight:"auto", padding: "0px 2.5px", marginLeft: "5px"}}>Name</p>
+                    <p>Symbol</p>
+                </div>
+                <div className="currency_data">
+                    <p>Price</p>
+                    <p>Volume</p>
+                    <p>
+                        Daily Change
+                    </p> 
+                    <p>
+                        Market Cap
+                    </p>
+                </div>
+            </div>
+      </div>
+      <div className="results_container">
+        {filteredCurrency.map(currency => {
+          return (
+            <Currency 
+            key={currency.id}  
+            name={currency.name}  
+            image={currency.image}  
+            symbol={currency.symbol}
+            marketCap={currency.market_cap}  
+            price={currency.current_price}  
+            priceChange={currency.price_change_percentage_24h}  
+            volume={currency.total_volume}  
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Currency.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function Currency(props) {
+    const volume_marketCap = [props.volume, props.marketCap]
+    let volume = "2"
+    let marketCap = "4"
+
+    volume_marketCap.forEach(data => {
+        let length = data.toString().length
+        let newData = ""
+        if (length > 6 && length < 10){
+            newData = (data / 1000000).toFixed(1)
+            newData = `${newData.toString()} M`
+        } else if (length > 9 && length < 13){
+            newData = (data / 1000000000).toFixed(1)
+            newData = `${newData.toString()} B`
+        }
+        if (data === props.volume){
+            volume = newData
+        } else {
+            marketCap = newData
+        }
+    })
+
+
+
+    return (
+        <div className="currency_container">
+            <div className="currency_row">
+                <div className="currency">
+                    <img src={props.image} alt="" />
+                    <h1>{props.name}</h1>
+                    <p>{props.symbol}</p>
+                </div>
+                <div className="currency_data">
+                    <p>${props.price.toLocaleString(undefined, {minimumFractionDigits: 2})}</p>
+                    <p>${volume.toLocaleString()}</p>
+                    {props.priceChange < 0 ? 
+                    <p className="change_neg">
+                        {props.priceChange.toFixed(2)}%
+                    </p> : props.priceChange > 0 ? 
+                    <p className="change_pos">
+                        {props.priceChange.toFixed(2)}%
+                    </p> : 
+                    <p>
+                        N/A
+                    </p>}
+                    <p>
+                        ${marketCap}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
